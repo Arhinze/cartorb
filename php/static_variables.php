3e7cbc485653c30135fc981f9c57a697fde5b818
Arinze <francisokoye48@gmail.com>
2025-10-29 02:47:16 +0100

    first commit on cartorb for upload

---
+++ php/static_variables.php
@@ -0,0 +1,17 @@
+<?php
+
+$site_name = "Cartorb";
+$site_url="https://cartorb.com";
+$site_url_short="cartorb.com";
+
+$site_color_dark = "#042c06";
+$site_color_light = "#ff9100";
+
+define("SITE_NAME", "Cartorb");
+define("SITE_NAME_SHORT", "Cartorb");
+define("SITE_URL", "https://cartorb.com");
+define("SITE_URL_SHORT", "cartorb.com");
+
+ini_set("display_errors", '1');
+date_default_timezone_set('Africa/Lagos');
+?>
